c230a64a318e2da4159aa765da8415eb1a87b406
Hanif <razlanhanif1@gmail.com>
2023-06-30 16:17:35 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+# I am learning about print and how to set variable and use the variables
+
+
+print("hello World")
+
+name = "jon"
+age = 12
+print("Hello " + name)
+print("Age is " + str(age))  # I am using number and covert to string by using str.
+
+name = "amin"
+print("My name is " + name)
+print("Age " + str(age))
+
+
+x = 12
+y = 10
+num = x - y
+print(num)
