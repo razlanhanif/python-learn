8f9ae334504d36c91c8becfa1076b47400c891b1
Hanif <razlanhanif1@gmail.com>
2023-07-02 19:10:49 +0800

    if else function

--- boolean.py
+++ boolean.py
@@ -1,2 +1,20 @@
 # learning about boolean
 
+a = 1000
+b = 999
+if b > a:
+    print("The number 999 is not greater then 1000")
+else:
+    print("1000 is greater then 999")
+
+# create a input function with if else statement
+num1 = input("Please input a number")
+num2 = input("Please input another number")
+result = num1 + num2
+
+if num1 > num2:
+    print(result)
+else:
+    print("Your first number is less then your second number!")
+
+
